3548e3ecfdb6afa94842b9cf03f25ecd563c9317
rommdev <dc.4231253@gmail.com>
2026-06-30 02:32:51 -0500

    first commit

---
+++ src/main/java/com/bandas/BandasRockApplication.java
@@ -0,0 +1,13 @@
+package com.bandas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandasRockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandasRockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandas/BandasRockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandasRockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
